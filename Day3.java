f97173dd3f1f4522f9889e0fa6e45d49cc444877
Andri <andrikurniawanm.12@gmail.com>
2025-09-10 07:06:15 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,10 @@
+public class Day3 {
+public static void main (String [] args) {
+  System.out.println("Hari ketiga"); //ini adalah print line
+  System.out.print("Membuat Komentar");
+  System.out.println("");
+  System.out.printf("| %-10s Selamat Datang|", "Pesan:");
+  /* Outputnya adalah
+    |Pesan:     Selamat Datang|*/
+}
+}
